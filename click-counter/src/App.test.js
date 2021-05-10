b51dbb2c4df9c30dfdb3a00eda595e0d4d0e1b9e
honghainguyen777 <n.h.hai2015@gmail.com>
2021-05-10 12:00:49 +0200

    testing button click leading the count incremented

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -32,4 +32,16 @@ test("counter display starts at 0", () => {
   expect(count).toBe("0");
 });
 
-test("click button increments counter display", () => {});
+test("click button increments counter display", () => {
+  const wrapper = setup();
+
+  // find the button
+  const button = findByTestAttr(wrapper, "increment-button");
+
+  // click the button
+  button.simulate("click");
+
+  // find the display, and test that the number has been incremented
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("1");
+});
